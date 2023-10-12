d9fd6050d748215a258ab0617966175d3e11f7ec
philipwanjiku <muhorophilip7@gmail.com>
2023-10-12 10:49:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+var mainElement = document.getElementById("main");
+if (mainElement) {
+    mainElement.parentNode.removeChild(mainElement);
+  }
+  
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent= "Philip is the champion"
+
